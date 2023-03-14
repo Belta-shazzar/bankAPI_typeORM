c3e35f8786b17d2ec6d200655ca4edf6ebb51ce7
Shazzar <nielnonso@gmail.com>
2023-03-14 09:46:47 +0100

    feat: add a feature to get all transaction receipt specific to a user

--- src/controllers/service/transaction.service.ts
+++ src/controllers/service/transaction.service.ts
@@ -2,6 +2,9 @@ import { TransactionType, TransactionStatus } from "./../../util/enums";
 import { User } from "../../entities/User";
 import { Account } from "../../entities/Account";
 import { Transaction } from "../../entities/Transaction";
+import { AppDataSource } from "../../config/data-source";
+
+const transactionRepository = AppDataSource.getRepository(Transaction)
 
 export const createTransactionReceipt = async (
   author: User,
@@ -24,4 +27,11 @@ export const createTransactionReceipt = async (
   } catch (error) {
     console.log(error)
   }
+};
+
+export const getAccountByOwnerId = async (authorId: number) => {
+  return await transactionRepository
+    .createQueryBuilder("transaction")
+    .where("transaction.authorId = :id", { id: authorId })
+    .getMany();
 };
--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -1,7 +1,26 @@
 import { Request, Response } from "express";
+import { AppDataSource } from "../config/data-source";
 import { Transaction } from "../entities/Transaction";
+import { errorResponse } from "../util/helper";
+import { StatusCodes } from "http-status-codes";
 
-export const getAllTransactionReceipt = async (req: Request, res: Response) => {
+const transactionRepository = AppDataSource.getRepository(Transaction)
+
+export const getAllTransactionReceipt = async (req: any, res: Response) => {
+
+try {
+  const transactions = await transactionRepository
+    .createQueryBuilder("transaction")
+    .where("transaction.authorId = :id", { id: req.user.userId })
+    .getMany();
+  
+  return res.status(StatusCodes.OK).json({ success: true, data: { transactions } })
+} catch (error) {
+  console.log(error);
+  let status = StatusCodes.INTERNAL_SERVER_ERROR;
+  let message = "something went wrong";
+  errorResponse(status, message, res);
+}
   console.log("All your transactions...");
   console.log();
 };
